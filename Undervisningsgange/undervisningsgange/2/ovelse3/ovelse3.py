8616045dedd852b6ac47d01cf9a81db96f454bb0
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-02-13 09:03:53 +0100

    not tested

--- Undervisningsgange/undervisningsgange/2/ovelse3/ovelse3.py
+++ Undervisningsgange/undervisningsgange/2/ovelse3/ovelse3.py
@@ -1,5 +1,8 @@
 import sqlite3
 import Adafruit_DHT
+import RTC
+from time import sleep
+
 
 conn = sqlite3.connect('dht11.db')
 
@@ -9,11 +12,20 @@ data = (Time, Temp, Humi)
 sensor = Adafruit_DHT.DHT11
 pin = 23
 
-Time = 
-Temp = temperature = Adafruit_DHT.read_retry(sensor, pin)
-Humi = humidity = Adafruit_DHT.read_retry(sensor, pin)
+rtc = RTC()
+while True:
 
+    Time = rtc.datetime()
+    Temp = temperature = Adafruit_DHT.read_retry(sensor, pin)
+    Humi = humidity = Adafruit_DHT.read_retry(sensor, pin)
+    sleep(10)
+    try:
+        cur = conn.cursor()
+        cur.execute(query, data)
+        conn.commit()
+    except sqlite3.Error as e:
+        conn.rollback()
+        print(f'Could not insert ! {e}')
 
-try:
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM airdata')
+    except KeyboardInterrupt:
+        conn.close()
