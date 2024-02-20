a3ada73a31f2fb66289394a32a9e78eff131f805
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-02-20 13:26:05 +0100

    html

--- Undervisningsgange/Prog2/HTML/Flask/app.py
+++ Undervisningsgange/Prog2/HTML/Flask/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route("/")
+    def index():
+        greeting = "Hello World"
+        return render_template("index.html", greeting=greeting)
