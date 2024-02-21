28cb1da90a6911f54abcc653ad28af66b288cd51
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-02-21 23:47:23 +0100

    øvelse 1 matplotlib done

--- Undervisningsgange/Prog2/matplotlib/øvelse1.py
+++ Undervisningsgange/Prog2/matplotlib/øvelse1.py
@@ -1,35 +1,32 @@
-import base64
-from io import BytesIO
-from flask import Flask
-from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import numpy as np
 
-app = Flask(__name__)
-@app.route("/")
-def hello():
+x1points = np.array([0, 2, 4, 6, 8, 10, 12, 14])
+y1points = np.array([0, 2, 8, 10, 4, 2, 9, 4])
+y2points = np.array([0, 8, 1, 3, 0, 10, 5, 4])
 
-    fig = Figure()
-    ax= fig.subplots()
-    y = [1, 8, 1, 7]
-    x = [1, 6, 9, 12]
-    ax.set_facecolor("#000") # inner plot background color HTML black
-    fig.patch.set_facecolor('#000') # outer plot background color HTML black
-    ax.plot(x, y, linestyle = 'dashed', c = '#11f', linewidth = '1.5',
-        marker = 'o', mec = 'hotpink', ms = 10, mfc = 'hotpink' )
-    ax.set_xlabel('X-axis ')
-    ax.set_ylabel('Y-axis ')
-    ax.xaxis.label.set_color('hotpink') #setting up X-axis label color to hotpink
-    ax.yaxis.label.set_color('hotpink') #setting up Y-axis label color to hotpink
-    ax.tick_params(axis='x', colors='white') #setting up X-axis tick color to white
-    ax.tick_params(axis='y', colors='white') #setting up Y-axis tick color to white
-    ax.spines['left'].set_color('blue') # setting up Y-axis tick color to blue
-    ax.spines['top'].set_color('blue') #setting up above X-axis tick color to blue
-    ax.spines['bottom'].set_color('blue') #setting up above X-axis tick color to blue
-    ax.spines['right'].set_color('blue') #setting up above X-axis tick color to 
+fig, ax = plt.subplots()
 
-    buf = BytesIO()
-    fig.savefig(buf, format="png")
+ax.set_facecolor("#000")
+fig.patch.set_facecolor('#250127') # outer plot background color HTML
 
-    data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    return f"<img src='data:image/png;base64,{data}'/>"
-if __name__ == "__main__":
-    app.run()
+ax.plot(x1points, y2points, linestyle = 'dotted', c = '#c800ff', linewidth = '2.0',
+    marker = 'D', mec = 'yellow', ms = 10, mfc = 'yellow' )
+ax.plot(x1points, y1points, linestyle = 'dashed', c = '#ffff', linewidth = '1.5',
+    marker = '*', mec = 'red', ms = 10, mfc = 'white' )
+
+ax.set_xlabel('X-axis ')
+ax.set_ylabel('Y-axis ')
+
+ax.xaxis.label.set_color('red') #setting up X-axis label color to hotpink
+ax.yaxis.label.set_color('Yellow') #setting up y-axis label color to hotpink
+
+ax.tick_params(axis='x', colors='red')
+ax.tick_params(axis='y', colors='yellow')
+
+ax.spines['left'].set_color('white') # setting up Y-axis tick color to blue
+ax.spines['top'].set_color('purple') #setting up above X-axis tick color to blue
+ax.spines['bottom'].set_color('white') #setting up above X-axis tick color to blue
+ax.spines['right'].set_color('purple') #setting up above X-axis tick color to blue
+
+plt.show()
