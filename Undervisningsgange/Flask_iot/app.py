e562383fd36f5cc58374483ddcc2405b3823815c
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-02-26 11:28:04 +0100

    tutorial

--- Undervisningsgange/Flask_iot/app.py
+++ Undervisningsgange/Flask_iot/app.py
@@ -1,9 +1,46 @@
+import base64
+from io import BytesIO
+from matplotlib.figure import Figure
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def stue_temp():
+    # Generate the figure **without using pyplot**.
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([1, 2])
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
+
+def stue_hum():
+    # Generate the figure **without using pyplot**.
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([2, 1])
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/stue')
+def stue():
+    stue_temperature = stue_temp()
+    stue_humidity = stue_hum()
+    return render_template('stue.html', stue_temperature=stue_temperature, stue_humidity=stue_humidity)
+
+@app.route('/Kokken')
+def køkken():
+    return render_template('kokken.html')
 
 app.run(debug=True)
